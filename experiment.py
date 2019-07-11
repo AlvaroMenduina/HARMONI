909f78977b9ebb439c30d81fb493775d3fae8235
AlvaroMenduina <alvaro.menduina@gmail.com>
2019-07-11 11:00:29 +0100

    Experiment

--- experiment.py
+++ experiment.py
@@ -19,9 +19,10 @@ from keras.models import Sequential, Model, Input
 from keras import backend as K
 from numpy.linalg import norm as norm
 
-N_zern = 5
-Z = 1.
-N_features = 25*25
+N_zern = 3
+Z = 1.25
+pix = 12
+N_features = pix**2
 
 class PointSpreadFunction(object):
     """
@@ -30,9 +31,9 @@ class PointSpreadFunction(object):
     """
 
     ### Parameters
-    rho_aper = 0.25         # Size of the aperture relative to 1.0
+    rho_aper = 0.40         # Size of the aperture relative to 1.0
     N_pix = 128
-    pix = 25                # Number of pixels for the Zoom of the PSF
+                   # Number of pixels for the Zoom of the PSF
     minPix, maxPix = (N_pix + 1 - pix) // 2, (N_pix + 1 + pix) // 2
 
     def __init__(self, N_zern):
@@ -105,24 +106,50 @@ class PointSpreadFunction(object):
         plt.colorbar()
         plt.clim(vmin=0, vmax=1)
 
-def generate_training_set(PSF_model, N_samples=1000):
+def generate_training_set(PSF_model, N_samples=1500):
 
     _im, s = PSF_model.compute_PSF(np.zeros(N_zern))
-    N_features = _im.flatten().shape[0]
+    extra_coefs = generate_sampling(2, N_zern, 0.2, -0.1)
+    N_features = _im.flatten().shape[0] * (extra_coefs.shape[0] + 1)
 
     training = np.zeros((N_samples, N_features))
     coefs = np.zeros((N_samples, N_zern))
+
+
     for i in range(N_samples):
         rand_coef = np.random.uniform(low=-Z, high=Z, size=N_zern)
         coefs[i] = rand_coef
         ims, _s = PSF_model.compute_PSF(rand_coef)
-        training[i] = ims.flatten()
+        nom_im = [ims.flatten()]
+        # print(ims.flatten().shape)
+        for c in extra_coefs:
+            ims, _s = PSF_model.compute_PSF(rand_coef + c)
+            nom_im.append(ims.flatten())
+            # print(len(nom_im))
+        training[i] = np.concatenate(nom_im)
 
     return training, coefs
 
+def generate_sampling(sampling, N_zern, delta, start=0.0):
+    """
+    Mimics the sampling of the Zernike coefficients from Zemax
+    It returns an array of [N_train, N_zern] that matches the PSFs
+    generated with POP in the ZPL Macro loops
+    """
+    coefs = np.empty((sampling**N_zern, N_zern))
+    for i in range(N_zern):
+        n = sampling ** (N_zern - (i + 1))
+        a = start * np.ones(n)
+        for j in range(sampling - 1):
+            b = (start + (j + 1) * delta) * np.ones(n)
+            a = np.concatenate((a, b))
+        index = np.tile(a, sampling ** i)
+        coefs[:, i] = index
+    return coefs
+
 
 if __name__ == "__main__":
-
+    extra_coefs = generate_sampling(2, N_zern, 0.2, -0.1)
     coef = np.random.uniform(low=-Z, high=Z, size=N_zern)
     print(coef)
 
@@ -130,40 +157,45 @@ if __name__ == "__main__":
     PSF.plot_PSF(coef, i=0)
     plt.show()
 
+    train_images, train_coefs = generate_training_set(PSF)
+
     from keras.backend.tensorflow_backend import tf
 
     model = Sequential()
-    model.add(Dense(N_features//125, input_shape=(N_features,), activation='relu'))
-    # model.add(Dense(N_features//5, input_shape=(N_features,), activation='relu'))
-    # model.add(Dense(N_features//25, input_shape=(N_features,), activation='relu'))
-    # model.add(Dense(N_features//125, input_shape=(N_features,), activation='relu'))
+    model.add(Dense(1000, input_shape=(train_images.shape[1],), activation='relu'))
+    model.add(Dense(500, activation='relu'))
+    model.add(Dense(100, activation='relu'))
+    model.add(Dense(50, activation='relu'))
+    model.add(Dense(N_zern, activation='relu'))
     model.summary()
 
-    H = PSF.H_matrix.copy()
+    H = PSF.H_matrix.copy().T
+    H = np.rollaxis(H, 1, 0)
     pup = PSF.pupil.copy()
     peak = PSF.PEAK.copy()
-    def strehl_loss(y_pred, y_true):
-        print(y_pred.shape)
 
-        phase = np.dot(H, y_pred)
-        print(phase.shape)
-        cos_x = pup * K.cos(phase)
-        sin_x = pup * K.sin(phase)
 
-        F_cos = tf.fft2d(cos_x)
-        F_sin = tf.fft2d(sin_x)
+    pupt = tf.constant(pup, dtype=tf.float32)
+    ht = tf.constant(H, dtype=tf.float32)
 
-        # pupil_function = pup * K.exp(1j * phase)
-        # image = (K.abs(tf.fft2d(pupil_function)))**2
-        # strehl = K.max(image)/peak
+    def loss(y_pred, y_true):
+        phase = K.dot(y_pred, ht)
+        print(y_pred)
+        cos_x = pupt * K.cos(phase)
+        sin_x = pupt * K.sin(phase)
+        complex_phase = tf.complex(cos_x, sin_x)
+        image = (K.abs(tf.fft2d(complex_phase)))**2
+        print(image.shape)
+        strehl = K.max(image, axis=(1, 2)) / peak
+        print(strehl.shape)
 
-        return 1.
+        return 0*y_true - K.mean(strehl)
 
-    model.compile(optimizer='adam', loss=strehl_loss)
-    #
-    # train_images, train_coefs = generate_training_set(PSF)
-    #
-    # model.fit(x=train_images, epochs=100, shuffle=True, verbose=2)
+    model.compile(optimizer='adam', loss=loss)
+
+    model.fit(x=train_images, y=train_coefs, epochs=5000, shuffle=True, verbose=1)
+
+    guess = model.predict(train_images[:10])
 
 
 
