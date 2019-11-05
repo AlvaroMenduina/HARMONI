d94625fa974b0b59bdd088fcf7ec0cb05745c1dc
AlvaroMenduina <alvaro.menduina@gmail.com>
2019-11-05 09:25:30 +0000

    LAM Update

--- Multiwave/multiwave.py
+++ Multiwave/multiwave.py
@@ -15,46 +15,14 @@ from numpy.linalg import norm as norm
 
 # PARAMETERS
 Z = 1.25                    # Strength of the aberrations
-pix = 25                    # Pixels to crop the PSF
+pix = 30                    # Pixels to crop the PSF
 N_PIX = 256
 RHO_APER = 0.5
 RHO_OBSC = 0.15
-N_WAVES = 5
+N_WAVES = 4
 WAVE_N = 1.6
 
 " Actuators "
-
-def actuator_centres(N_actuators, rho_aper=RHO_APER, rho_obsc=RHO_OBSC,
-                     N_waves=N_WAVES, wave0=1.0, waveN=WAVE_N):
-    """
-    Computes the (Xc, Yc) coordinates of actuator centres
-    inside a circle of rho_aper, assuming there are N_actuators
-    along the [-1, 1] line
-
-    :param N_actuators:
-    :param rho_aper:
-    :return:
-    """
-    waves_ratio = np.linspace(1., waveN/wave0, N_waves, endpoint=True)
-
-    centres = []
-    for wave in waves_ratio:
-        x0 = np.linspace(-1./wave, 1./wave, N_actuators, endpoint=True)
-        delta = x0[1] - x0[0]
-        xx, yy = np.meshgrid(x0, x0)
-        x_f = xx.flatten()
-        y_f = yy.flatten()
-
-        act = []
-        for x_c, y_c in zip(x_f, y_f):
-            r = np.sqrt(x_c ** 2 + y_c ** 2)
-            if r < 0.95 * rho_aper / wave and r > 1.1 * rho_obsc /wave:
-                act.append([x_c, y_c])
-
-        total_act = len(act)
-        print('Total Actuators: ', total_act)
-        centres.append([act, delta])
-    return centres
 
 def actuator_centres(N_actuators, rho_aper=RHO_APER, rho_obsc=RHO_OBSC,
                      N_waves=N_WAVES, wave0=1.0, waveN=WAVE_N, radial=True):
@@ -226,11 +194,15 @@ def generate_training_set(PSF_model, N_train=1500, N_test=500):
 
 if __name__ == "__main__":
 
+
+    plt.rc('font', family='serif')
+    plt.rc('text', usetex=False)
+
     wave0 = 1.0
     waves_ratio = np.linspace(1., WAVE_N / wave0, N_WAVES, endpoint=True)
 
     N_actuators = 25
-    centers = actuator_centres(N_actuators)
+    centers = actuator_centres(N_actuators, radial=False)
     N_act = len(centers[0][0])
 
     for i, wave_r in enumerate(waves_ratio):
@@ -280,24 +252,68 @@ if __name__ == "__main__":
 
     plt.show()
 
+    # ================================================================================================================ #
+    """ Could the Multiwavelength information be useful for calibration? """
+    ### The Answer is OF COURSE NOT
+    coeff_degen = np.zeros(N_act)
+    k_act = 25
+    coeff_degen[k_act] = 15
+    coeff_degen[-k_act] = 15
+    phase_degen = np.dot(rbf_matrices[0][0], coeff_degen)
+
+    plt.figure()
+    plt.imshow(phase_degen)
+    plt.colorbar()
+    plt.show()
+    for wave_idx in range(N_WAVES):
+        p_plus, _s = PSF.compute_PSF(coeff_degen, wave_idx)
+        coeff_degen[-1] *= -1
+        p_minus, _s = PSF.compute_PSF(coeff_degen, wave_idx)
+
+        mapp = 'viridis'
+
+        f, (ax1, ax2, ax3) = plt.subplots(1, 3)
+        ax1 = plt.subplot(1, 3, 1)
+        img1 = ax1.imshow(p_plus, cmap=mapp)
+        ax1.set_title('Plus')
+        # img1.set_clim(m, -m)
+        plt.colorbar(img1, ax=ax1, orientation='horizontal')
+
+        ax2 = plt.subplot(1, 3, 2)
+        img2 = ax2.imshow(p_minus, cmap=mapp)
+        ax2.set_title('Minus')
+        # img2.set_clim(m, -m)
+        plt.colorbar(img2, ax=ax2, orientation='horizontal')
+
+        ax3 = plt.subplot(1, 3, 3)
+        img3 = ax3.imshow(p_plus - p_minus, cmap=mapp)
+        ax3.set_title('Diff')
+        # img3.set_clim(m, -m)
+        plt.colorbar(img3, ax=ax3, orientation='horizontal')
+    plt.show()
+
     # ------------------------------------------------------------------------------- #
 
-    N_train, N_test = 15000, 100
+    N_train, N_test = 5000, 500
     training_PSF, test_PSF, training_coef, test_coef = generate_training_set(PSF, N_train, N_test)
 
-    # plt.figure()
-    # plt.imshow(training_PSF[0,:,:,4], cmap='hot')
-    # plt.colorbar()
-    #
-    # plt.figure()
-    # plt.imshow(training_PSF[0,:,:,5], cmap='hot')
-    # plt.colorbar()
-    #
-    # plt.show()
+    k_train = 0
+    plot_waves = 3
+    f, axes = plt.subplots(plot_waves, 2)
+    for i in range(plot_waves):
+        ax = plt.subplot(plot_waves, 2, 2*i + 1)
+        PSF_nom = training_PSF[k_train, :,:,2*i]
+        img = ax.imshow(PSF_nom)
+        ax.set_title('Nominal PSF [Wave %.2f]' %waves_ratio[i])
 
-    from keras.regularizers import l2
+        ax = plt.subplot(plot_waves, 2, 2*i + 2)
+        PSF_nom = training_PSF[k_train, :,:,2*i+1]
+        img = ax.imshow(PSF_nom)
+        ax.set_title('Defocus PSF [Wave %.2f]' %waves_ratio[i])
+    plt.show()
 
-    N_channels = 2*N_WAVES
+    waves_considered=1
+    N_channels = 2*waves_considered
     input_shape = (pix, pix, N_channels,)
 
     model = Sequential()
@@ -307,22 +323,181 @@ if __name__ == "__main__":
     model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
     model.add(Conv2D(64, (3, 3), activation='relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
-    # model.add(Conv2D(32, (3, 3), activation='relu'))
     model.add(Flatten())
     model.add(Dense(N_act))
-    # model.add(Dense(N_act, kernel_regularizer=l2(0.01)))
     model.summary()
 
-
     model.compile(optimizer='adam', loss='mean_squared_error')
-    train_history = model.fit(x=training_PSF, y=training_coef,
-                              validation_data=(test_PSF, test_coef),
+    train_history = model.fit(x=training_PSF[:,:,:,:2*waves_considered], y=training_coef,
+                              validation_data=(test_PSF[:,:,:,:2*waves_considered], test_coef),
                               epochs=250, batch_size=32, shuffle=True, verbose=1)
     loss_hist = train_history.history['loss']
     val_hist = train_history.history['val_loss']
 
-    guess = model.predict(test_PSF)
+    guess = model.predict(test_PSF[:,:,:,:2*waves_considered])
     residual = test_coef - guess
+
+    def test_models(PSF_model, training_PSF, test_PSF, training_coef, test_coef):
+
+        """
+        Check the influence of the number of Wavelength channels
+        on the performance of the algorithm
+
+        :param training_PSF:
+        :param test_PSF:
+        :param training_coef:
+        :param test_coef:
+        :return:
+        """
+        N_test = test_PSF.shape[0]
+        N_waves = training_PSF.shape[-1] // 2
+        list_waves = list(np.arange(1, N_waves + 1))
+
+        # Evaluate the Initial RMS and Wavefronts
+        rbf_mat = PSF_model.RBF_mat[0]
+        pupil_mask = PSF_model.pupil_masks[0]
+        rms0, rms = [], []
+        wavefr0, wavefronts = [], []
+        for k in range(N_test):
+            phase0 = np.dot(rbf_mat, test_coef[k])
+            wavefr0.append(phase0)
+            phase0_f = phase0[pupil_mask]
+            rms0.append(np.std(phase0_f))
+
+        validation_losses = []
+        guessed_coef = []
+
+        for waves in list_waves:
+
+            print("\nConsidering %d Wavelengths" %waves)
+
+            N_channels = 2 * waves
+            input_shape = (pix, pix, N_channels,)
+            print(input_shape)
+            model = Sequential()
+            model.add(Conv2D(32, kernel_size=(3, 3), strides=(1, 1), activation='relu', input_shape=input_shape))
+            model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+            model.add(Conv2D(64, (3, 3), activation='relu'))
+            model.add(MaxPooling2D(pool_size=(2, 2)))
+            model.add(Flatten())
+            model.add(Dense(N_act))
+            model.summary()
+            model.compile(optimizer='adam', loss='mean_squared_error')
+
+            # Slice Channels
+            train_wave = training_PSF[:, :, :, :N_channels]
+            test_wave = test_PSF[:, :, :, :N_channels]
+
+            train_history = model.fit(x=train_wave, y=training_coef,
+                                      validation_data=(test_wave, test_coef),
+                                      epochs=10, batch_size=32, shuffle=True, verbose=1)
+            loss_hist = train_history.history['loss']
+            val_hist = train_history.history['val_loss']
+            validation_losses.append(val_hist)
+
+            guess = model.predict(test_wave)
+            guessed_coef.append(guess)
+            residual = test_coef - guess
+
+
+            # Check the RMS wavefront error for each case
+            wavefr = []
+            _rms = []
+            for k in range(N_test):
+                phase = np.dot(rbf_mat, residual[k])
+                wavefr.append(phase)
+                phase_f = phase[pupil_mask]
+                _rms.append(np.std(phase_f))
+            wavefronts.append(wavefr)
+            rms.append(_rms)
+
+        plt.figure()
+        for i in range(N_waves):
+            plt.plot(validation_losses[i], label=list_waves[i])
+        plt.xlabel('Iterations')
+        plt.ylabel('Loss')
+        plt.legend(title='Channels')
+
+        return validation_losses, guessed_coef, rms0, rms, wavefr0, wavefronts
+
+    validation_losses, guessed_coef, rms0, rms, wavefr0, wavefronts = test_models(PSF, training_PSF, test_PSF,
+                                                                             training_coef, test_coef)
+
+    list_waves = list(np.arange(1, N_WAVES + 1))
+
+    plot_waves = 4
+    n_rows = plot_waves // 2
+    f, axes = plt.subplots(n_rows, n_rows)
+    for i in range(n_rows):
+        for j in range(n_rows):
+            k = n_rows * i + j
+            rms_wave = rms[k]
+            avg_rms = np.mean(rms_wave)
+            med_rms = np.median(rms_wave)
+            std_rms = np.std(rms_wave)
+            print(k)
+            ax = plt.subplot(n_rows, n_rows, k + 1)
+            ax.hist(rms0, histtype='step')
+            ax.hist(rms_wave, histtype='step')
+            ax.axvline(med_rms, linestyle='--', color='black')
+            ax.set_xlim([0, 1])
+            ax.set_ylim([0, 250])
+            ax.set_title(r'Wave Channels %d | RMW WFE %.3f ($\sigma$=%.3f)' % (list_waves[k], avg_rms, std_rms))
+    plt.show()
+
+    k_phase = 5
+    plot_waves = 4
+    mapp = 'bwr'
+    f, axes = plt.subplots(1, plot_waves + 1)
+    # Initial Wavefront
+    phase0 = wavefr0[k_phase]
+    pmin = min(np.min(phase0), -np.max(phase0))
+    ax = plt.subplot(1, plot_waves + 1, 1)
+    img = ax.imshow(phase0, cmap=mapp)
+    img.set_clim(pmin, -pmin)
+    ax.set_title(r'Initial Wavefront %.4f' % rms0[k_phase])
+    plt.colorbar(img, ax=ax, orientation='horizontal')
+
+    for k in range(plot_waves):     # Loop over the
+        phase = wavefronts[k][k_phase]
+        ax = plt.subplot(1, plot_waves + 1, k+2)
+        img = ax.imshow(phase, cmap=mapp, label='A')
+        img.set_clim(pmin, -pmin)
+        ax.set_title(r'Residual %.4f [%d Waves]' % (rms[k][k_phase], list_waves[k]))
+        plt.colorbar(img, ax=ax, orientation='horizontal')
+
+    # plt.legend()
+    plt.show()
+
+
+    """ What if we include noise in the training? """
+
+    def noise_images(dataset, coef, RMS_READ, N_copies=3):
+        N_PSF, pix, _pix, N_chan = dataset.shape
+        N_act = coef.shape[-1]
+        new_data = np.empty((N_copies * N_PSF, pix, pix, N_chan))
+        new_coef = np.empty((N_copies * N_PSF, N_act))
+
+        for k in range(N_PSF):
+            PSF = dataset[k].copy()
+            coef_copy = coef[k].copy()
+            for i in range(N_copies):
+                read_out = np.random.normal(loc=0, scale=RMS_READ, size=(pix, pix, N_chan))
+
+                new_data[N_copies * k + i] = PSF + read_out
+                new_coef[N_copies * k + i] = coef_copy
+
+        return new_data, new_coef
+
+    RMS_READ = 1./100
+    read_train_PSF, read_train_coef = noise_images(training_PSF, training_coef, RMS_READ, N_copies=3)
+    read_test_PSF, read_test_coef = noise_images(test_PSF, test_coef, RMS_READ, N_copies=3)
+
+    ### Check the performance on noisy data
+    val_loss, guessed_coef, rms0, rms, wavefr0, wavefronts = test_models(PSF, read_train_PSF, read_test_PSF,
+                                                                         read_train_coef, read_test_coef)
+
+
 
 
     def compute_RMS(test_images, true_coef, mask=PSF.pupil_masks[0]):
